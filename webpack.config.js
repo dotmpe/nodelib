3cd7f55b88f832659dc792886ac4677d10486fd9
B. van Berkum <dev@dotmpe.com>
2016-11-25 00:55:03 +0100

    Further tuning webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,62 @@
+const webpack = require('webpack');
+const fs = require('fs');
 
-module.exports = {
-  entry: [ './src/node/index.coffee' ],
-  output: {
-    filename: 'dist/nodelib.js'
-  },
-	module: {
-		loaders: [
-			{ test: /\.coffee$/, loader: "coffee" },
-			{ test: /\.json$/, loader: "json" }
-		]
-	},
-	resolve: {
+const config = {}
+
+config.entry = [ './src/node/index.coffee' ]
+
+// Node module dependencies should not be bundled
+config.externals = fs.readdirSync("node_modules")
+  .reduce(function(acc, mod) {
+    if (mod === ".bin") {
+      return acc
+    }
+
+    acc[mod] = "commonjs " + mod
+    return acc
+  }, {})
+
+config.target = 'node'
+
+//config.node = {
+//  markdown: "empty",
+//  "bookshelfapi": "empty"
+//}
+
+config.output = {
+  filename: 'dist/nodelib.js'
+}
+
+config.module = {}
+
+config.module.loaders = [
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loaders: [
+				"babel?{stage:0,jsxPragma:'this.createElement'}",
+				"eslint",
+			],
+		},
+		{ test: /\.coffee$/, loader: "coffee" },
+	]
+
+config.resolve = {
 		extensions: [
 		  "", ".coffee", ".js"
 		]
-	},
-	/* Allow Node.JS globals */
-  //  process: "empty",
-	/* Allow Node.JS stdlib */
-  node: {
-    fs: "empty",
-    child_process: "empty",
-    net: "empty"
-  },
-}
+	}
+
+config.plugins = [
+  new webpack.BannerPlugin('require("source-map-support").install();',
+                                   { raw: true, entryOnly: false }),
+  new webpack.IgnorePlugin(/markdown|pug|stylus|pm2|pmx|knex|bookshelfapi/)
+]
+
+config.devtool = 'sourcemap'
+
+
+module.exports = config
+
+
 
